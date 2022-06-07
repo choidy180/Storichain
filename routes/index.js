c43cdb1c38b14860d07c60e6c9e1ab5dc76ac3fb
choidy180 <choidy180@naver.com>
2022-06-07 15:04:30 +0900

    commot

--- routes/index.js
+++ routes/index.js
@@ -9,25 +9,29 @@ router.get('/topup', function(req, res, next) {
   res.render('index', { 
     title: 'Express', 
     page: "cash/topup.ejs",
-    balance: "5,000,222,111"
+    balance: "5,000,222,111",
+    head: "TORI Cash Topup"
   });
 });
 router.get('/checkout', function(req, res, next) {
   res.render('index', { 
     title: 'Express', 
     page: "cash/checkout.ejs",
+    head: "구매확인 및 결제방법",
   });
 });
 router.get('/paymentdone', function(req, res, next) {
   res.render('index', { 
     title: 'Express', 
     page: "cash/paymentdone.ejs",
+    head: "결제완료"
   });
 });
 router.get('/cashwallet', function(req, res, next) {
   res.render('index', { 
     title: 'Express', 
     page: "cash/cashwallet.ejs",
+    head: "나의 캐시 지갑"
   });
 });
 module.exports = router;
